3e1a6ebf76c7f13343c3201d26ab6b8960e65630
Ray <30580339+uigleki@users.noreply.github.com>
2026-01-22 11:51:40 +0800

    chore: remove non-Dart VSCode settings and extensions

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [ flutter ];
+      };
+    };
+}
